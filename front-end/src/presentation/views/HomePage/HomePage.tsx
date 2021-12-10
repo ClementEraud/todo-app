325719b8279dbef7b1ab4477900c0fc04dc33db9
Clément Eraud <clement.eraud@gmail.com>
2021-12-11 00:50:07 +0100

    💄 front-end(Theme): fix dark theme on HomePage

--- front-end/src/presentation/views/HomePage/HomePage.tsx
+++ front-end/src/presentation/views/HomePage/HomePage.tsx
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import { ThemeProvider, createTheme } from '@mui/material/styles';
 import { Avatar } from '@mui/material';
 import Box from '@mui/material/Box';
 import CssBaseline from '@mui/material/CssBaseline';
@@ -8,8 +7,6 @@ import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import { Outlet } from 'react-router-dom';
 import Paper from '@mui/material/Paper';
 
-const theme = createTheme();
-
 const HomePage = () => {
 	const imageLink = () => {
 		const rnd = Math.floor(Math.random() * (1 - 6) + 6);
@@ -17,42 +14,40 @@ const HomePage = () => {
 	};
 
 	return (
-		<ThemeProvider theme={theme}>
-			<Grid container component="main" sx={{ height: '100vh' }}>
-				<CssBaseline />
-				<Grid
-					item
-					xs={false}
-					sm={4}
-					md={7}
+		<Grid container component="main" sx={{ height: '100vh' }}>
+			<CssBaseline />
+			<Grid
+				item
+				xs={false}
+				sm={4}
+				md={7}
+				sx={{
+					backgroundImage: `url(${imageLink()})`,
+					backgroundRepeat: 'no-repeat',
+					backgroundColor: t =>
+						t.palette.mode === 'light'
+							? t.palette.grey[50]
+							: t.palette.grey[900],
+					backgroundSize: 'cover',
+					backgroundPosition: 'center',
+				}}
+			/>
+			<Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+				<Box
 					sx={{
-						backgroundImage: `url(${imageLink()})`,
-						backgroundRepeat: 'no-repeat',
-						backgroundColor: t =>
-							t.palette.mode === 'light'
-								? t.palette.grey[50]
-								: t.palette.grey[900],
-						backgroundSize: 'cover',
-						backgroundPosition: 'center',
-					}}
-				/>
-				<Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
-					<Box
-						sx={{
-							my: 8,
-							mx: 4,
-							display: 'flex',
-							flexDirection: 'column',
-							alignItems: 'center',
-						}}>
-						<Avatar sx={{ m: 1, bgcolor: 'primary.main' }}>
-							<LockOutlinedIcon />
-						</Avatar>
-						<Outlet />
-					</Box>
-				</Grid>
+						my: 8,
+						mx: 4,
+						display: 'flex',
+						flexDirection: 'column',
+						alignItems: 'center',
+					}}>
+					<Avatar sx={{ m: 1, bgcolor: 'primary.main' }}>
+						<LockOutlinedIcon />
+					</Avatar>
+					<Outlet />
+				</Box>
 			</Grid>
-		</ThemeProvider>
+		</Grid>
 	);
 };
 
