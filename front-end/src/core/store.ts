c32969b1ec4f82656c297447e866df4adb7a72bc
Clément Eraud <clement.eraud@gmail.com>
2022-03-04 17:44:26 +0100

    👷 test ci launch only front job

--- front-end/src/core/store.ts
+++ front-end/src/core/store.ts
@@ -1,7 +1,6 @@
 import { IStore } from './interfaces/Store.interface';
 import { Store } from 'pullstate';
 
-// try adding some dumb comment
 export const store = new Store<IStore>({
 	isDarkMode: true,
 });
