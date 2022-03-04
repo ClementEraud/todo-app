f7eae6f9007d867c1c276ee23c75eb252a431d62
Clément Eraud <clement.eraud@gmail.com>
2022-03-04 16:25:30 +0100

    👷 add conditions for workflows

--- front-end/src/core/store.ts
+++ front-end/src/core/store.ts
@@ -1,6 +1,7 @@
 import { IStore } from './interfaces/Store.interface';
 import { Store } from 'pullstate';
 
+// try adding some dumb comment
 export const store = new Store<IStore>({
 	isDarkMode: true,
 });
