ce0aa0aee092e7bc0d7d767017059917eaebeac8
Clément Eraud <clement.eraud@thetribe.io>
2022-06-24 23:17:52 +0200

    :green_heart: fix docker-compose

--- front-end/src/presentation/CustomLoader.tsx
+++ front-end/src/presentation/CustomLoader.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
+import { BallTriangle } from 'react-loader-spinner';
 import Box from '@mui/material/Box';
-import Loader from 'react-loader-spinner';
 
 export const CustomLoader = () => (
 	<Box
@@ -10,12 +10,6 @@ export const CustomLoader = () => (
 			left: '50%',
 			transform: 'translate(-50%, -50%)',
 		}}>
-		<Loader
-			type="BallTriangle"
-			color="#00BFFF"
-			height={100}
-			width={100}
-			timeout={3000} //3 secs
-		/>
+		<BallTriangle color="#00BFFF" height={100} width={100} />
 	</Box>
 );
