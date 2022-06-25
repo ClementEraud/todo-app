56005dc49a053c1ed20a940941c07a9d62395174
Clément Eraud <clement.eraud@thetribe.io>
2022-06-25 09:38:23 +0200

    :green_heart: fix api env variables

--- back-end/src/app.module.ts
+++ back-end/src/app.module.ts
@@ -1,12 +1,39 @@
 import { APP_FILTER } from '@nestjs/core';
 import { ExceptionsFilter } from './consumers/rest_api/filters/ExceptionsFilter';
+import { LoggerOptions } from 'typeorm';
 import { MealPlannerModule } from './consumed/ioc/mealPlanner.module';
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from './consumed/ioc/user.module';
 
+const {
+	TYPEORM_HOST,
+	TYPEORM_USERNAME,
+	TYPEORM_PASSWORD,
+	TYPEORM_DATABASE,
+	TYPEORM_PORT,
+	TYPEORM_LOGGING,
+	TYPEORM_ENTITIES,
+	TYPEORM_MIGRATIONS,
+	TYPEORM_MIGRATIONS_RUN,
+	TYPEORM_SYNCHRONIZE } = process.env;
+
 @Module({
-	imports: [UserModule, MealPlannerModule, TypeOrmModule.forRoot()],
+	imports: [UserModule, MealPlannerModule, TypeOrmModule.forRoot(
+		{
+			type: 'mysql',
+			host: TYPEORM_HOST,
+			username: TYPEORM_USERNAME,
+			password: TYPEORM_PASSWORD,
+			database: TYPEORM_DATABASE,
+			port: Number(TYPEORM_PORT),
+			logging: TYPEORM_LOGGING as LoggerOptions,
+			entities: [TYPEORM_ENTITIES],
+			migrations: [TYPEORM_MIGRATIONS],
+			migrationsRun: Boolean(TYPEORM_MIGRATIONS_RUN),
+			synchronize: Boolean(TYPEORM_SYNCHRONIZE)
+		}
+	)],
 	providers: [
 		{
 			provide: APP_FILTER,
